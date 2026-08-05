b9494a66528175e1b13e75afd7097346095c03d3
chuakx-wm23-ux <chuakx-wm23@student.tarc.edu.my>
2026-08-05 22:13:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1911,8 +1911,8 @@ def make_batch_excel_bytes(dataframe):
         pie_buffer.seek(0)
 
         pie_image = XLImage(pie_buffer)
-        pie_image.width = 405
-        pie_image.height = 260
+        pie_image.width = 410
+        pie_image.height = 251
         dashboard_sheet.add_image(
             pie_image,
             "F11",
